f79b1092974d5323c8c41db497eb92924848742b
Edy <146010238+edyburr@users.noreply.github.com>
2024-08-19 12:04:52 +0300

    Create fetch.py

---
+++ .github/script/fetch.py
@@ -0,0 +1,23 @@
+import http.client
+import json
+
+conn = http.client.HTTPSConnection("get-population.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "RAPIDAPI_KEY",
+    'x-rapidapi-host': "get-population.p.rapidapi.com"
+}
+
+conn.request("GET", "/population", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+# Decode and parse the JSON response
+parsed_data = json.loads(data.decode("utf-8"))
+
+# Write the parsed JSON data to a file
+with open('data/population.json', 'w') as f:
+    json.dump(parsed_data, f, indent=2)
+
+print("API data fetched and saved.")
